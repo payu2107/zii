fc25dab17c6bada39fb4114e43c94f29da318322
sebathi <sebathi@128d4e36-5ccc-11de-8c10-5fe6993ea61f>
2009-10-05 15:03:07 +0000

    Adding $id keyword

--- widgets/jui/CJuiSliderInput.php
+++ widgets/jui/CJuiSliderInput.php
@@ -40,7 +40,7 @@ Yii::import('zii.widgets.jui.CJuiInputWidget');
  * for possible options (name-value pairs).
  *
  * @author Sebastian Thierer <sebathi@gmail.com>
- * @version $Id: CJuiSliderInput 30 2009-09-25 19:22:50Z qiang.xue $
+ * @version $Id$
  * @package zii.widgets.jui
  * @since 1.1
  */
