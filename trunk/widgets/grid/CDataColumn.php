535fd901b29859fb77db2346d4fa649b2361d22a
qiang.xue <qiang.xue@128d4e36-5ccc-11de-8c10-5fe6993ea61f>
2010-06-17 16:07:23 +0000

    Added support to auto-generate header label based on name.

--- trunk/widgets/grid/CDataColumn.php
+++ trunk/widgets/grid/CDataColumn.php
@@ -109,8 +109,14 @@ class CDataColumn extends CGridColumn
 	{
 		if($this->grid->enableSorting && $this->sortable && $this->name!==null)
 			echo $this->grid->dataProvider->getSort()->link($this->name,$this->header);
-		else
-			parent::renderHeaderCellContent();
+		else if($this->name!==null && $this->header===null)
+		{
+			if($this->dataProvider instanceof CActiveDataProvider)
+				echo CHtml::encode($this->dataProvider->model->getAttributeLabel($this->name));
+			else
+				echo CHtml::encode($this->name);
+		}
+		parent::renderHeaderCellContent();
 	}
 
 	/**
